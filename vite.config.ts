5a1bc84dbc0517ec3ad2fb8e36f025b664d9a621
Juninho21 <106665230+Juninho21@users.noreply.github.com>
2025-02-09 14:43:05 -0300

    Corrigir base path no vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/safeprag_new/',
+  base: '/safeprag_site/',
   server: {
     port: 3000,
     host: true
